fa6094c1eb3f919543a7c2f6f433036321f3e7b2
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-12-30 23:56:55 +0330

    Code refactor

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,9 +1,9 @@
-import notes from "../apis/map";
+import map from "../apis/map";
 import { USER_REGISTER } from "../constants";
 import { User } from "../components/Auth";
 
 export const registerUser = (user: User) => async (dispatch: any) => {
-  const response = await notes.post("/user", user);
+  const response = await map.post("/user", user);
 
   dispatch({
     type: USER_REGISTER,
